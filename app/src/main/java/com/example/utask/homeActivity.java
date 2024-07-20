0b30689c402d8d98941af84f86b45fd4bba5a252
Drunkenjin3 <ares.ydeo.up@phinmaed.com>
2024-07-21 00:05:10 +0800

    added toast, fixed shared preferences

--- app/src/main/java/com/example/utask/homeActivity.java
+++ app/src/main/java/com/example/utask/homeActivity.java
@@ -1,10 +1,12 @@
 package com.example.utask;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
-
+import android.content.SharedPreferences;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,12 +14,21 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class homeActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Retrieve data from SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        String name = sharedPreferences.getString("name", "No name");
+        String email = sharedPreferences.getString("email", "No email");
+        String school = sharedPreferences.getString("school", "No school");
+        String num = sharedPreferences.getString("num", "No number");
+        String studNum = sharedPreferences.getString("studNum", "No student number");
+
+        // Display data using Toast
+        Toast.makeText(this, "Welcome, " + name + "!", Toast.LENGTH_SHORT).show();
 
         //bottom nav//
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -26,40 +37,37 @@ public class homeActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.profile:
-                        startActivity(new Intent(getApplicationContext(),profileActivity.class));
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext(), profileActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.home:
                         return true;
-                    case  R.id.create:
-                        startActivity(new Intent(getApplicationContext(),createActivity.class));
-                        overridePendingTransition(0,0);
+                    case R.id.create:
+                        startActivity(new Intent(getApplicationContext(), createActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
-                    case  R.id.complete:
-                        startActivity(new Intent(getApplicationContext(),completeActivity.class));
-                        overridePendingTransition(0,0);
+                    case R.id.complete:
+                        startActivity(new Intent(getApplicationContext(), completeActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
                 }
                 return false;
             }
         });
 
+        // Update TextViews with user data
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView nameTextView = findViewById(R.id.nameTextView);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView emailTextView = findViewById(R.id.emailTextView);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView schoolTextView = findViewById(R.id.schoolTextView);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView numTextView = findViewById(R.id.numTextView);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView studNumTextView = findViewById(R.id.studNumTextView);
 
-
-
-
-        //bundle shit//
-        Bundle bundle = getIntent().getExtras();
-        if (bundle != null){
-            String name = bundle.getString("name");
-            String email = bundle.getString("email");
-            String school_name = bundle.getString("univ_name");
-            String contact_num = bundle.getString("contact_num");
-            String student_num = bundle.getString("stud_num");
-            Toast.makeText(this, "Welcome!", Toast.LENGTH_SHORT).show();
-        }
-
+        nameTextView.setText(name);
+        emailTextView.setText(email);
+        schoolTextView.setText(school);
+        numTextView.setText(num);
+        studNumTextView.setText(studNum);
     }
 }
